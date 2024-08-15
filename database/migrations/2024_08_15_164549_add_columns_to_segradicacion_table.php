4c044342f58808e81e52546813c440593d14b7ab
2023Harold <harromrom.osfem@gmail.com>
2024-08-15 17:53:49 -0600

    Se agregan nuevos campos a la tabla de radicación.DMM

---
+++ database/migrations/2024_08_15_164549_add_columns_to_segradicacion_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('segradicacion', function (Blueprint $table) {
+            $table->text('num_memo_recepcion_expediente',200)->nullable();
+            $table->date('fecha_expediente_turnado')->nullable();
+            $table->date('fecha_oficio_informe')->nullable();
+            $table->date('fecha_notificacion')->nullable();
+            $table->integer('plazo_maximo')->nullable();
+            $table->date('calculo_fecha')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('segradicacion', function (Blueprint $table) {
+            //
+        });
+    }
+};
